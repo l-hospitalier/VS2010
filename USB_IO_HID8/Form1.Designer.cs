c95e6a576790a805005a33b86823db2692e46fe4
l-hospitalier <kouichi.tamiya@gmail.com>
2021-04-06 08:57:14 +0900

    Add files via upload

---
+++ USB_IO_HID8/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace USB_IO_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(399, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "USB-IO_FS30 AD converter";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(16, 91);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(781, 464);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(15, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "START/STOP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(252, 25);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 39);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(780, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(17, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 571);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ USB_IO_HID8/Form1.cs
@@ -0,0 +1,190 @@
+﻿using HidLibrary;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace USB_IO_8   // for Km2net USB-IO FS-30  A/D Converter
+{
+    public partial class Form1 : Form
+    {
+        const int duration_1 = 25;
+        static object sync = new object();
+
+        int[] ch = new int[25];
+        double[] Adata = new double[9];
+
+        USBIO_Device device = USBIO_Device.Connect();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void WriteAD()
+        {
+            int j = 2;
+            double n = 5.0 / 1023;
+
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[0] = ch[j] * n;
+
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[1] = ch[j] * n;
+
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[2] = ch[j] * n;
+
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[3] = ch[j] * n;
+            //
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[4] = ch[j] * n;
+
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[5] = ch[j] * n;
+
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[6] = ch[j] * n;
+
+            j = j + 3;
+            ch[j] = USBIO_Device.receiveData[j + 1] + USBIO_Device.receiveData[j + 2] << 2;
+            Adata[7] = ch[j] * n;
+
+            for (int i = 1; i < 9; i++) textBox1.Text += Adata[i - 1].ToString("f2") + "V  ";
+
+            textBox1.Text += "\r\n";
+
+            textBox1.HideSelection = false;
+            //カレット位置を末尾に移動
+            textBox1.SelectionStart = textBox1.Text.Length;
+            //テキストボックスにフォーカスを移動
+            textBox1.Focus();
+            //カレット位置までスクロール
+            textBox1.ScrollToCaret();
+        }
+
+        public void WriteData()
+        {
+            for (int i = 0; i < 26; i++)
+                textBox1.Text += USBIO_Device.receiveData[i].ToString("X2") + " ";
+            textBox1.Text += "\r\n";
+        }
+
+        public void WriteDeviceList()
+        {
+
+            var deviceList = HidDevices.Enumerate().ToArray();
+
+            foreach (var data in deviceList) textBox1.Text += data.ToString();
+            // for (int i = 0; i < deviceList.Length; i++) textBox1.Text +=  deviceList[i].ToString();
+
+            textBox1.Text += "\r\n";
+
+            textBox1.HideSelection = false;
+            //カレット位置を末尾に移動
+            textBox1.SelectionStart = textBox1.Text.Length;
+            //テキストボックスにフォーカスを移動
+            textBox1.Focus();
+            //カレット位置までスクロール
+            textBox1.ScrollToCaret();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            device.SendReceive();
+      
+            //if (this.InvokeRequired) Invoke((MethodInvoker)delegate() { WriteData(); });
+            //else WriteData();
+      
+            if (this.InvokeRequired) Invoke((MethodInvoker)delegate(){WriteAD();});
+            else WriteAD();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WriteDeviceList();
+        }
+    }
+
+    public class USBIO_Device : IDisposable
+    {
+        private const int VendorID = 0x1352;
+        private const int ProductID = 0x0111; // FS-30
+
+        public static byte[] receiveData = new byte[64];   // byte 配列の定義 
+        public static byte[] sendData = new byte[64];
+
+        bool disposed = false;
+        public readonly HidDevice device;
+
+     //   public static HidDevice[] ListDevices() => HidDevices.Enumerate(VendorID, ProductID).ToArray();
+     //   public static USBIO_Device Connect(HidDevice device) => new USBIO_Device(device);
+
+        public static USBIO_Device Connect()
+        {
+            var device = HidDevices.Enumerate(VendorID, ProductID).FirstOrDefault();
+            if (device == null)
+            {
+                throw new InvalidOperationException("Deviceが見つかりません");
+            }
+            return new USBIO_Device(device);
+        }
+
+        public USBIO_Device(HidDevice device)
+        {
+            this.device = device;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);    // Dispose of unmanaged resources.
+            GC.SuppressFinalize(this);    // Suppress finalization.
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing) device.Dispose();
+            disposed = true;
+        }
+
+        public void SendReceive()
+        {
+            // var sendData = new byte[64];  // A/D コマンド 
+            sendData[1] = 0x2A;  // A/D コマンド 
+            sendData[2] = 0x01;  // 1ch
+            sendData[5] = 0x02;
+            sendData[8] = 0x03;
+            sendData[11] = 0x04;
+            sendData[14] = 0x05;
+            sendData[17] = 0x06;
+            sendData[20] = 0x07;
+            sendData[23] = 0x08;
+
+            var res = device.Write(sendData);
+            if (!res) throw new InvalidOperationException("送信に失敗しました");
+
+            var rec = device.Read();
+            if (rec.Status != HidDeviceData.ReadStatus.Success)
+                throw new InvalidOperationException($"受信に失敗しました: {rec.Status}");
+
+            for (int i = 0; i < 64; i++) receiveData[i] = rec.Data[i];　　// receiveData に格納
+        }
+    }
+}
